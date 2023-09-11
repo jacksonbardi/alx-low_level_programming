e9fcf1c037ac19c28c8388de3e45e0bb5c03ea08
jacksonbardi <rachadwafir8@gmail.com>
2023-09-11 10:05:05 -0700

    task_advanced-two

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Succes)
+ * main - prints all possible different combinations of three digits.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        int n, m, l;
-        
-        for (n = 48; n < 58; n++)
-                for (m = 49; m < 58; m++)
-                        for (l = 50; l < 58; l++)
-                                if (l > m && m > n)
-                                        putchar(n);
-                                        putchar(m);
-                                        putchar(l);
-                                        if (n != 55 || m != 56)
-                                        {       
-                                                putchar(',');
-                                                putchar(' ');
-                                        }       
-                                 }      
-                        }
-                }
-         }
-         putchar('\n');
-         return (0);
-}	 
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
