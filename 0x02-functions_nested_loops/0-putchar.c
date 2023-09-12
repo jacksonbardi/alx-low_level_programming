78093721d1afd000a175464088996b4ea79bb6e2
jacksonbardi <rachadwafir8@gmail.com>
2023-09-12 12:55:36 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
-/***
- * main - this is a function
- * Return: returns 0
- */
+/**
+  * main -  prints _putchar
+  *
+  * Return: return 0
+  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-return (0);
+	return (0);
 }
 
