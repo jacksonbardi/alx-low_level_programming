42c7ab34c0db3abe70cbc4be07a530949c66bf83
jacksonbardi <rachadwafir8@gmail.com>
2023-09-25 13:44:13 -0700

    7-task

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_chessboard - prints the chessboard.
+  * @a: is a pointer to an array of 8 characters.
+  * Return: Always 0 (success)
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
