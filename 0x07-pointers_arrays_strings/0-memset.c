27f3cc24b695f0ab2be593aaa7381fc79b14366f
jacksonbardi <rachadwafir8@gmail.com>
2023-09-25 13:04:31 -0700

    0-task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * *_memset - fills memory with a constant byte.
+  * @n: the size of the memory to print
+  * @b: character
+  * @s: the address of memory to print
+  * Return: a pointer to the memory area s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
