20b7b051c950e59a85f5856da7e99286e6f9851e
jacksonbardi <rachadwafir8@gmail.com>
2023-10-11 13:24:07 -0700

    0-task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print - prints a name
+ * @name: name to print
+ */
+void print(char *name)
+{
+	printf("Name: %s\n", name);
+}
+
+/**
+ * print_name - prints a name by calling printing function passed as parameter
+ * @name: name to print
+ * @f: function to call to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
