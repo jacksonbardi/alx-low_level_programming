db1cad91ea577d29a6e672afbf97765a4aaef28b
jacksonbardi <rachadwafir8@gmail.com>
2023-10-26 14:17:23 -0700

    4-task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the vlue of a bit to 0 at a given index.
+ * @n: Pointer to the nmbr to clear the bit in.
+ * @index: The index of the bit to clear.
+ *
+ * Rturn: 1 if it worked, or -1 if an error occurred.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = ~(1UL << index);
+	*n &= mask;
+
+	return (1);
+}
